7b0428b82911c0740c6e9d31eaea60fa4149cb2b
hhkk <hank.khang@gmail.com>
2018-11-25 22:50:49 -0500

    df
    df

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const { graphql, buildSchema } = require('graphql')
+const graphqlHTTP = require('express-graphql')
+const cors = require('cors')
+
+const schema = buildSchema(`
+  type Query {
+    language: String
+  }
+`)
+
+
+
+
+const rootValue = {
+    language: () => 'GraphQL'
+}
+
+const app = express()
+app.use(cors())
+app.use('/graphql', graphqlHTTP({
+    rootValue, schema, graphiql: true
+}))
+
+
+
+app.listen(4001, () => console.log('Listening on 4001'))
+
